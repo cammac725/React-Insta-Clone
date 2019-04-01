3570b2a96446dd30d008f522b0bd3b4396c735be
cammac <camm@rocktechcomputer.com>
2019-04-01 12:41:28 -0700

    Insta-Cam initial

---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class PostContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <h1>Post Container</h1>
+    )
+  }
+}
+
+export default PostContainer;
---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchBar = props => {
+  return (
+    <h1>Search Bar</h1>
+  )
+}
+
+export default SearchBar;
